668432480ebdfed987a26247f5c91e61948edd84
Vijay <AM.Vijay@gmail.com>
2019-09-25 10:55:16 -0500

    environment variable setup details.

---
+++ environment-variable-setup.cmd
@@ -0,0 +1,10 @@
+#Author VIJAY (AM.Vijay@gmail.com)
+
+set HADOOP_HOME=C:\Users\135670\Vijay\hadoop\hadoop-3.2.0
+set JAVA_HOME=C:\Users\135670\Vijay\JDK\jdk1.8.0_212
+
+
+set HADOOP_PREFIX=%HADOOP_HOME%
+set HADOOP_CONF_DIR=%HADOOP_PREFIX%\etc\hadoop
+set YARN_CONF_DIR=%HADOOP_CONF_DIR%
+set PATH=%PATH%;%JAVA_HOME%\bin;%HADOOP_HOME%\bin;%HADOOP_HOME\sbin;
