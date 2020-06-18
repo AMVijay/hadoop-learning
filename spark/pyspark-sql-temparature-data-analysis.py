81073158a521f15a24173e11e6c4d557708817f7
Vijay <AM.Vijay@gmail.com>
2020-06-18 15:08:15 -0500

    Updated temparature analysis - understanding.

--- spark/pyspark-sql-temparature-data-analysis.py
+++ spark/pyspark-sql-temparature-data-analysis.py
@@ -18,7 +18,7 @@ minSqlDataframe.show()
 results = minSqlDataframe.collect()
 
 for result in results:
-    print(result[0 + "\t{:.2f}F".format(result[1]))
+    print(result[0] + "\t{:.2f}F".format(result[1]))
 
 # Find maximum temparature using SQL where condition, min operation, group by.
 maxSqlDataframe = sparkSession.sql("select t._c0, max(t._c3*0.1*(9.0/5.0) + 32.0) as MAX_TEMPARATURE from temparature t where t._c2='TMAX' group by t._c0")
@@ -27,3 +27,11 @@ results = maxSqlDataframe.collect()
 
 for result in results:
     print(result[0] + "\t{:.2f}F".format(result[1]))
+
+## Convert the column _c3 from celcius to Farenheit and double value
+dataframe = dataframe.withColumn("_c3",dataframe["_c3"] * 0.1*(9/5)+32)
+## Print the schema now
+dataframe.printSchema()
+
+## Perform the max temparature find using filter, groupby, max methods available in dataframe.
+dataframe.filter(dataframe["_c2"] == "TMAX").groupBy("_c0").max("_c3").show()
